326d3471d93d07be757859c30efff7ec65c16a38
John Abdellmelk <jnabdell@uncg.edu>
2023-03-20 10:34:54 -0400

    getting ready to test on internet

---
+++ app.js
@@ -0,0 +1,11 @@
+<script>
+    $(document).ready(function() {
+        $('.slideshow').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            adaptiveHeight: true
+        });
+    });
+</script>
