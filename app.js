44dc02f06d4b486473c9b6c63a3858ebdec95e6d
John Abdellmelk <JNABDELL@uncg.edu>
2023-03-21 13:18:36 -0400

    testing slideshow

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-<script>
+
     $(document).ready(function() {
         $('.slideshow').slick({
             dots: true,
@@ -8,4 +8,31 @@
             adaptiveHeight: true
         });
     });
-</script>
+    let slideIndex = 1;
+    showSlides(slideIndex);
+    
+    // Next/previous controls
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+    
+    // Thumbnail image controls
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+    
+    function showSlides(n) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += " active";
+    }
