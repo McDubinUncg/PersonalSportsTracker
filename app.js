75dadde2119538658dfe8e176aa85efc45c6a78b
John Abdellmelk <jnabdell@uncg.edu>
2023-03-21 21:32:38 -0400

    testing a news api

---
+++ app.js
@@ -0,0 +1,8 @@
+fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=8845aff61b5844de9a896fcddbe33d5e')
+  .then(response => response.json())
+  .then(data => {
+    const article = data.articles[0];
+    const headline = article.title;
+    document.getElementById('headline').textContent = headline;
+  })
+  .catch(error => console.error(error));
