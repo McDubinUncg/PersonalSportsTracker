24fee2ca7be7249633094e4135a764223397fbff
John Abdellmelk <jnabdell@uncg.edu>
2023-03-23 21:43:22 -0400

    made the login button a modal

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Fetch news data
 fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=8845aff61b5844de9a896fcddbe33d5e')
   .then(response => response.json())
   .then(data => {
@@ -7,6 +8,7 @@ fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=8845aff61b5844de9a
   })
   .catch(error => console.error(error));
 
+// Netlify Identity configuration
 if (window.netlifyIdentity) {
   window.netlifyIdentity.on("init", user => {
     if (!user) {
@@ -16,3 +18,16 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+// Login modal
+const loginBtn = document.getElementById('login-btn');
+const loginModal = document.getElementById('login-modal');
+const closeBtn = document.querySelector('.w3-modal-content span');
+
+loginBtn.addEventListener('click', () => {
+  loginModal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+  loginModal.style.display = 'none';
+});
