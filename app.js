4d54de0541d335eef663093c7e1c3413d8b3a825
John Abdellmelk <jnabdell@uncg.edu>
2023-03-23 22:30:10 -0400

    slideshow on home

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-// Fetch news data
-fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=8845aff61b5844de9a896fcddbe33d5e')
-  .then(response => response.json())
-  .then(data => {
-    const article = data.articles[0];
-    const headline = article.title;
-    document.getElementById('headline').textContent = headline;
-  })
-  .catch(error => console.error(error));
-
 // Netlify Identity configuration
 if (window.netlifyIdentity) {
   window.netlifyIdentity.on("init", user => {
